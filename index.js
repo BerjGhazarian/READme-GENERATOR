a20028e0ed370b0747397917f69f34f11c901709
berj ghazarian <ghazarian_berj@hotmail.com>
2022-03-21 17:37:50 -0700

    added more questions into the array

--- index.js
+++ index.js
@@ -20,6 +20,32 @@ const questions = [
         message: "Give a description of your project.",
         name: "description",
       },
+      {
+        type: "input",
+        message: "Input installation guidelines",
+        name: "installation",
+      },
+      {
+        type: "input",
+        message: "Insert instructions for use",
+        name: "usage",
+      },
+      {
+        type: "list",
+        message: "Use arrow keys to select license options",
+        name: "license",
+        choices: ["MIT", "Apache"],
+      },
+      {
+        type: "input",
+        message: "List the names of everyone who contributed to this project.",
+        name: "contribute",
+      },
+      {
+        type: "input",
+        message: "Write the test guidelines for your project",
+        name: "test",
+      },
      
 ];
 
